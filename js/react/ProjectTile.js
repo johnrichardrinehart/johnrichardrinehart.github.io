ccc3b8b97a28adb732b2e56f6d3ba20c85a1447c
John Rinehart <johnrichardrinehart@gmail.com>
2019-02-02 12:41:32 -0800

    project description dropdown with GitHub link working

--- js/react/ProjectTile.js
+++ js/react/ProjectTile.js
@@ -1,15 +1,35 @@
 var e = React.createElement;
 
 var ProjectTile = function ProjectTile(_ref) {
-    var title = _ref.title;
+    var title = _ref.title,
+        desc = _ref.desc,
+        url = _ref.url;
 
     return React.createElement(
         "div",
         { "class": "card" },
         React.createElement(
-            "p",
+            "span",
             { "class": "title" },
             title
+        ),
+        React.createElement(
+            "span",
+            { "class": "desc-container" },
+            React.createElement(
+                "p",
+                { "class": "desc-text" },
+                desc
+            ),
+            React.createElement(
+                "p",
+                { "class": "desc-link" },
+                React.createElement(
+                    "a",
+                    { href: url },
+                    React.createElement("img", { src: "assets/GitHub-Mark/PNG/GitHub-Mark-64px.png", alt: "GitHub" })
+                )
+            )
         )
     );
 };
@@ -18,8 +38,8 @@ var ProjectsContainer = function ProjectsContainer() {
     return React.createElement(
         "div",
         null,
-        React.createElement(ProjectTile, { title: "Some Words" }),
-        React.createElement(ProjectTile, { title: "Some Other Words" })
+        React.createElement(ProjectTile, { title: "DMC Scheduler", desc: "DMC Scheduler is a nurse rostering tool written for the nurses of Doctor's Medical Center of Modesto.", url: "https://github.com/johnrichardrinehart/DMCScheduler.py" }),
+        React.createElement(ProjectTile, { title: "Some Other Words", desc: "hey" })
     );
 };
 
--- react/ProjectTile.jsx
+++ react/ProjectTile.jsx
@@ -1,9 +1,17 @@
 var e = React.createElement;
 
-var ProjectTile = function ProjectTile({ title }) {
+var ProjectTile = function ProjectTile({ title, desc, url }) {
     return (
         <div class="card">
-            <p class="title">{title}</p>
+            <span class="title">{title}</span>
+            <span class="desc-container">
+                <p class="desc-text">{desc}</p>
+                <p class="desc-link">
+                    <a href={url}>
+                        <img src="assets/GitHub-Mark/PNG/GitHub-Mark-64px.png" alt="GitHub" />
+                    </a>
+                </p>
+            </span>
         </div>
     )
 };
@@ -11,8 +19,8 @@ var ProjectTile = function ProjectTile({ title }) {
 var ProjectsContainer = function ProjectsContainer() {
     return (
         <div>
-            <ProjectTile title="Some Words"></ProjectTile>
-            <ProjectTile title="Some Other Words"></ProjectTile>
+            <ProjectTile title="DMC Scheduler" desc="DMC Scheduler is a nurse rostering tool written for the nurses of Doctor's Medical Center of Modesto." url="https://github.com/johnrichardrinehart/DMCScheduler.py"></ProjectTile>
+            <ProjectTile title="Some Other Words" desc='hey'></ProjectTile>
         </div>
     )
 };
